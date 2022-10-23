a780ae030f8b6a233f87d5bd6d5c341f45bda71e
samara <saruufan@gmail.com>
2022-10-23 13:14:20 -0500

    1

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-2"
 } 
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+}
