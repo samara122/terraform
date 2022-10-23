0ac5b8ccfd008bddcf1593b06eab3f736347850a
samara <saruufan@gmail.com>
2022-10-23 13:11:40 -0500

    adding

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-  ami           = "${var.myami}"
-  instance_type = "${var.myinstance_type}"
+  ami           = var.myami
+  instance_type = var.myinstance_type
 
   tags = {
     Name = "HelloWorld"
